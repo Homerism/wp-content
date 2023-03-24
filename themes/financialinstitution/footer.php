ba068089d1d0af8d95792f2750bfc9b2697ffad4
JDHomere <jahmel.homer@my.uwi.edu>
2023-03-24 13:47:27 -0400

    update

--- themes/financialinstitution/footer.php
+++ themes/financialinstitution/footer.php
@@ -27,7 +27,113 @@
 <script src="<?php echo get_theme_file_uri().'/js/nivo-lightbox.min.js' ?>"></script>
 <script src="<?php echo get_theme_file_uri().'/js/jquery.backstretch.min.js' ?>"></script>
 <script src="<?php echo get_theme_file_uri().'/js/wow.min.js' ?>"></script>
-<script src="<?php echo get_theme_file_uri().'/js/custom.js' ?>"></script>
+<script>
+
+	// ISOTOPE FILTER
+	jQuery(document).ready(function($){
+
+	if ( $('.iso-box-wrapper').length > 0 ) { 
+
+		var $container  = $('.iso-box-wrapper'), 
+		$imgs     = $('.iso-box img');
+
+		$container.imagesLoaded(function () {
+
+		$container.isotope({
+		layoutMode: 'fitRows',
+		itemSelector: '.iso-box'
+		});
+
+		$imgs.load(function(){
+			$container.isotope('reLayout');
+		})
+
+		});
+
+		//filter items on button click
+
+		$('.filter-wrapper li a').click(function(){
+
+			var $this = $(this), filterValue = $this.attr('data-filter');
+
+		$container.isotope({ 
+		filter: filterValue,
+		animationOptions: { 
+			duration: 750, 
+			easing: 'linear', 
+			queue: false, 
+		}                
+		});             
+
+		// don't proceed if already selected 
+
+		if ( $this.hasClass('selected') ) { 
+		return false; 
+		}
+
+		var filter_wrapper = $this.closest('.filter-wrapper');
+		filter_wrapper.find('.selected').removeClass('selected');
+		$this.addClass('selected');
+
+		return false;
+		}); 
+
+	}
+
+	});
+
+
+	// PRELOADER JS
+	$(window).load(function(){
+	$('.preloader').fadeOut(1000); // set duration in brackets    
+	});
+
+
+	// jQuery to collapse the navbar on scroll //
+	$(window).scroll(function() {
+	if ($(".navbar").offset().top > 50) {
+		$(".navbar-fixed-top").addClass("top-nav-collapse");
+	} else {
+		$(".navbar-fixed-top").removeClass("top-nav-collapse");
+	}
+	});
+
+
+	/* HTML document is loaded. DOM is ready. 
+	-------------------------------------------*/
+	$(function(){
+
+	// ------- WOW ANIMATED ------ //
+	wow = new WOW(
+	{
+	mobile: false
+	});
+	wow.init();
+
+
+	// HIDE MOBILE MENU AFTER CLIKING ON A LINK
+	$('.navbar-collapse a').click(function(){
+		$(".navbar-collapse").collapse('hide');
+	});
+
+
+	// NIVO LIGHTBOX
+	$('.iso-box-section a').nivoLightbox({
+		effect: 'fadeScale',
+	});
+
+
+	// HOME BACKGROUND SLIDESHOW
+	$(function(){
+	jQuery(document).ready(function() {
+	$('#home').backstretch([
+		"<?php echo get_theme_file_uri().'/images/home-bg-slideshow1.jpg' ?>", 
+		"<?php echo get_theme_file_uri().'/images/home-bg-slideshow2.jpg' ?>",
+		],  {duration: 2000, fade: 750});
+	});
+	})
+	});
+</script>
 
 </body>
 </html>
